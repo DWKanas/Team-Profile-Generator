14a36b41b70206bc1425ceea290f3d3bf4a15578
DWKanas <dkanas0122@gmail.com>
2022-06-25 21:42:32 -0700

    added requires and inquirer

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("test")
+const inquirer = require('inquirer'); 
+const fs = require('fs'); 
+const Manager = require('./lib/manager');  
+const Engineer = require('./lib/engineer'); 
+const Intern = require('./lib/intern');
